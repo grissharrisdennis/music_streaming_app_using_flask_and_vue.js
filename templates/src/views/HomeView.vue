99d7a8b12d056a6435b23809713a0206436193b5
grissharrisdennis <21f3001987@student.onlinedegree.iitm.ac.in>
2023-11-29 17:57:49 +0530

    home

--- templates/src/views/HomeView.vue
+++ templates/src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <NavBar></NavBar> 
   <div class="home">
+    <img src="../assets/ani.jpg">
+    <img src="../assets/kk.jpg">
+    <img src="../assets/chitra.jpg">
   </div>
 </template>
 
@@ -14,12 +17,46 @@ export default {
 }
 </script>
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 .home{
-  height:800px;
-  background: url('../assets/ani.jpg'),url('../assets/kk.jpg'),url('../assets/chitra.jpg');
-  background-repeat: no-repeat,no-repeat;
-  padding: 15px;
-  background-position: left,center,right;
-  background-size: contain;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+}
+@keyframes fade {
+  0%{
+    opacity:1;
+  }
+  33%{
+    opacity:0;
+  }
+  66%{
+    opacity:0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+img{
+  width:100%;
+  height:100%;
+  object-fit: cover;
+  position: absolute;
+  top:0;
+  left:0;
+  animation:fade 9s ease-in-out infinite alternate;
+}
+img:nth-of-type(1){
+  animation-delay:0s;
+}
+img:nth-of-type(2){
+  animation-delay:3s;
+}
+img:nth-of-type(3){
+  animation-delay:6s;
 }
 </style>
